62ed5b2c4180549d1d01dd40db1f41e4d7fc7947
Rokszin Roland <rokszinroland@gmail.com>
2025-01-27 09:01:30 +0100

    agreements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { TermsComponent } from './terms/terms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'privacy-policy', component: PrivacypolicyComponent },
+  { path: 'impressum', component: ImpressumComponent },
+  { path: 'terms-and-conditions', component: TermsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { SnowComponent } from './snow/snow.component';
+import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { HomeComponent } from './home/home.component';
+import { TermsComponent } from './terms/terms.component';
 
 //Angular-Materials
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import { ReactiveFormsModule} from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import { ObserveDirective } from './observe.directive';
-import {
-  MatBottomSheetModule
-} from '@angular/material/bottom-sheet';
-import { SnowComponent } from './snow/snow.component';
-
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 
 @NgModule({
   declarations: [
     AppComponent,
     ObserveDirective,
-    SnowComponent
+    SnowComponent,
+    PrivacypolicyComponent,
+    ImpressumComponent,
+    HomeComponent,
+    TermsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
 
     //Angular-materials
     MatIconModule,
@@ -38,12 +44,9 @@ import { SnowComponent } from './snow/snow.component';
     MatInputModule,
     ReactiveFormsModule,
     MatCardModule,
-    MatBottomSheetModule
-    
+    MatBottomSheetModule,
   ],
-  providers: [
-    provideAnimationsAsync(),
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
